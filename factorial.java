9c77f6594e41a3463861ed5f2c9b5b746a1950a7
Jay shinde <jay554261@gmail.com>
2025-04-20 00:24:02 +0530

    factoral

---
+++ factorial.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class factorial {
+
+
+
+    public static void fact(int num,int factorial) {
+       if (num == 0 || num == 1){
+        System.out.println(factorial);
+        return;
+       }
+       factorial *= num;
+       fact(num-1,factorial);
+    }
+    public static int facto(int num,int factorial) {
+        if (num == 0 || num == 1){
+        
+         return factorial;
+        }
+        
+        return facto(num-1,factorial*num);
+     }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int factorial =1;
+        System.out.println(facto(num,factorial));
+        fact(num,factorial);
+       
+    }
+
+
+}
